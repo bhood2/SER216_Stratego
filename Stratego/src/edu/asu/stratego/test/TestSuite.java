e510c51b598f41caad17ab01437604180db5ef96
Paul Hood <hoodp@mail.gvsu.edu>
2017-08-09 02:19:03 -0400

    Updated test suite

--- Stratego/src/edu/asu/stratego/test/TestSuite.java
+++ Stratego/src/edu/asu/stratego/test/TestSuite.java
@@ -10,8 +10,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ GameUnitTests.class, SpecialUnitTest.class, BombTest.class, ServerGameManagerTest.class,
+@SuiteClasses({ TestGame.class, TestPlayer.class, GameUnitTests.class, 
+	SpecialUnitTest.class, BombTest.class, ServerGameManagerTest.class,
 	ClientGameManagerTest.class})
-public class TestSuite {
-
-}
+public class TestSuite {}
